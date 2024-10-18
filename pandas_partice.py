9480d9b79f334f53c8cc9f73748ecc539f9a9968
Noa <ohknoa@gmail.com>
2024-10-18 22:55:43 +0530

    Series with List

--- pandas_partice.py
+++ pandas_partice.py
@@ -3,6 +3,21 @@ import pandas as pd
 
 #using the pandas with the list ds of python with series ds of pandas
 
-name = ["Aman","Ram","Noa","Ganesh"]
+#name = ["Aman","Ram","Noa","Ganesh"]
 
-print(pd.Series(name))
+#print(pd.Series(name))
+
+#example of months 
+
+#months =["jan","feb","march","april","may","june,","july","august","sep","oct","nov","dec"]
+
+#print(pd.Series(months))
+
+# using the index 
+
+runs = [59,32,30,88]
+batsman = ['Rohit',"Virat","Hardik","Rinku"]
+
+batsman_info = pd.Series(runs,index=batsman,name="Every Batsman runs ")
+
+print(batsman_info)
