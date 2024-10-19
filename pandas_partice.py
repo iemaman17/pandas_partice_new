643e888436fe2db4cff0985a7ec17d9cc4841ced
Noa <ohknoa@gmail.com>
2024-10-20 01:04:34 +0530

    Poting in the Series

--- pandas_partice.py
+++ pandas_partice.py
@@ -1,18 +1,16 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# series with the python functionalities 
+# ploting the graphs in the series 
 
 runs = pd.read_csv(r"C:\Users\NOA\Downloads\kohli_ipl.csv",index_col="match_no")
 
 if len(runs.columns)==1:
-    runs =runs.squeeze("columns")
+    runs = runs.squeeze("columns")
 
-#for m,s in runs.items():
-    #if s>50:
-       # count+=1
-        #print(f'{m} ={s}')
-
+#runs[runs>80].plot()
+#print(plt.show())
 
 movie = pd.read_csv(r"C:\Users\NOA\Downloads\bollywood.csv",index_col="movie")
 
@@ -20,6 +18,8 @@ if len(movie.columns)==1:
     movie =movie.squeeze("columns")
 
 a = movie.value_counts()
-a = a[a>20]
-
+a = a[a>15]
 print(a)
+
+a.plot(kind="pie")
+print(plt.show())
