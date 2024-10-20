3e5c73690fb8607bc85d139c6e4d4ba84a7b5513
Noa <ohknoa@gmail.com>
2024-10-20 18:50:17 +0530

    Rows OUtoFF dataframes

--- pandas_partice.py
+++ pandas_partice.py
@@ -2,15 +2,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# taking out the single columns from the dataframe
+# taking out the rows in the dataframes 
 
-movies = pd.read_csv(r"C:\Users\NOA\Downloads\movies.csv")
-print(movies)
+students = {
+    "Name":["Aman","jayesh","Ram",'Noa'],
+    "Marks":[89,55,48,54],
+    "Course":["Panda","Matplotlib","Java","AIML"],
+    "IQ":[190,89,97,48]
+}
+s = pd.DataFrame(students)
+s.set_index("Name",inplace=True)
+print(s)
+print(s.iloc[0:3]) # using the slicing indexing 
+print(s.iloc[[0,2,3]]) # this is the fancy indexing 
 
-a = movies["title_x"] 
-print(a.head())
-
-# taking out the more than one columns from the dataframe
-
-b = movies[["title_x","release_date","year_of_release"]]
-print(b.sample(3))
+print(s)
+print(s.loc[["Aman","Noa"]]) #fancy indexing with the lables
+print(s.loc["Aman":"Ram"])
