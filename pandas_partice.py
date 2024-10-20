7872900ef77f982052402ec0f566bd669d14fce2
Noa <ohknoa@gmail.com>
2024-10-20 18:29:36 +0530

    Columns OutOff DataFrame

--- pandas_partice.py
+++ pandas_partice.py
@@ -1,31 +1,16 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# maing the dataframes using the list 
-student_data = [
-    ["Aman",17,"Pandas"],
-    ["Noa",1,"PostgresQl"],
-    ["Ram",3,"Java"],
-    ["Jayesh",20,"Matplotlib"]
-]
-
-columns_data = ["Name","Fav_no","Cousre"]
-
-a = pd.DataFrame(student_data,columns=columns_data,index=[1,2,3,4])
-
-# making the dataframes uing the dictionaries 
-
-country_info = {
-    "Country":["USA","India","China","Russia"],
-    "Militry_Rank":[1,4,3,2],
-    "Population":["34 cr","140 Cr","130 Cr","14 Cr"],
-    "Status":["Rich","Medium","Rich","Rich"]
-}
-
-b = pd.DataFrame(country_info)
-
-# usually we imprt the data in real world
+# taking out the single columns from the dataframe
 
 movies = pd.read_csv(r"C:\Users\NOA\Downloads\movies.csv")
+print(movies)
 
-ipl = pd.read_csv(r"C:\Users\NOA\Downloads\ipl-matches.csv")
+a = movies["title_x"] 
+print(a.head())
+
+# taking out the more than one columns from the dataframe
+
+b = movies[["title_x","release_date","year_of_release"]]
+print(b.sample(3))
